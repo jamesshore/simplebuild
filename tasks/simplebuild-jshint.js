c81910603c0b4b123e3980cd0e299c2ac5de218e
James Shore <jshore@jamesshore.com>
2013-12-02 12:22:31 -0800

    Added JSHint task

---
+++ tasks/simplebuild-jshint.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+"use strict";
+
+var jshint = require("./jshint/jshint_runner.js");
+
+exports.validate = function(options, success, failure) {
+	var result = jshint.validateFileList(options.files, options.options, options.globals);
+	if (result) success();
+	else failure("JSHint found errors");
+};
+
+exports.validate.title = "JSHint";
+exports.validate.description = "Lint files using JSHint.";
